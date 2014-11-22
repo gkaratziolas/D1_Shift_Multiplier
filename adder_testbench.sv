8a1dd6cb5a424d139f952d209e5469ab98e8ec7e
Giorgos Karatziolas <gkaratziolas@gmail.com>
2014-11-22 13:31:47 +0000

    working adder and adder testbench

--- adder_testbench.sv
+++ adder_testbench.sv
@@ -1,17 +1,28 @@
-module test_adder;
+module adder_testbench;
 
     parameter n = 4;
 
     logic[n-1:0] A, M, Sum;
-    logic clock, c;
-
-    initial
-      begin
-        clock = 1'b0;
-        forever #10ns clock = ~clock;
-      end
-
-
+    logic C;
 
     adder ADDER (.*);
+
+
+    always
+      begin
+	A = 0;
+        M = 0;
+	for (A=0; A<2**n-1; A++)
+          begin
+            for (M=0; M<2**n-1; M++)
+              begin
+                #2ns;
+                assert( {C, Sum} == A + M )
+                else
+                    $error("Fail: Not Working!");
+              end
+          end
+	$display("Adder works for all values");
+      end
+
 endmodule
